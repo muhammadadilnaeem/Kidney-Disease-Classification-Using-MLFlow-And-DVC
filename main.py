16593dff4fcc5c8dced748d5ff5309553b9d44f7
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-29 20:08:07 +0000

    main file

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 
 from kidney_disease_classifier import logger
 from kidney_disease_classifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
-# from kidney_disease_classifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from kidney_disease_classifier.pipeline.stage_3_model_training import ModelTrainingPipeline
+from kidney_disease_classifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from kidney_disease_classifier.pipeline.stage_3_model_training import ModelTrainingPipeline
 # from kidney_disease_classifier.pipeline.stage_4_model_evaluation import EvaluationPipeline
 
 # Define the name of the current stage in the data processing pipeline
@@ -25,44 +25,44 @@ except Exception as e:
         raise e  # Reraise the exception for further handling if necessary
 
 
-# # Define the name of the current stage in the data processing pipeline
-# STAGE_NAME = "Prepare Base Model stage"
+# Define the name of the current stage in the data processing pipeline
+STAGE_NAME = "Prepare Base Model stage"
 
-# try:
-#         # Log the start of the stage
-#         logger.info(f"*******************")
-#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+try:
+        # Log the start of the stage
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         
-#         # Create an instance of the training pipeline and run the main method
-#         prepare_base_model = PrepareBaseModelTrainingPipeline()
-#         prepare_base_model.main()
+        # Create an instance of the training pipeline and run the main method
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
         
-#         # Log the completion of the stage
-#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         # Log any exceptions that occur during the execution
-#         logger.exception(e)
-#         raise e  # Reraise the exception for further handling
+        # Log the completion of the stage
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        # Log any exceptions that occur during the execution
+        logger.exception(e)
+        raise e  # Reraise the exception for further handling
 
 
-# # Define the name of the current stage in the data processing pipeline
-# STAGE_NAME = "Model Training stage"
+# Define the name of the current stage in the data processing pipeline
+STAGE_NAME = "Model Training stage"
 
-# try:
-#         # Log the start of the stage
-#         logger.info(f"*******************")
-#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+try:
+        # Log the start of the stage
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         
-#         # Create an instance of the training pipeline and run the main method
-#         model_trainer = ModelTrainingPipeline()
-#         model_trainer.main()
+        # Create an instance of the training pipeline and run the main method
+        model_trainer = ModelTrainingPipeline()
+        model_trainer.main()
         
-#         # Log the completion of the stage
-#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         # Log any exceptions that occur during the execution
-#         logger.exception(e)
-#         raise e  # Reraise the exception for further handling
+        # Log the completion of the stage
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        # Log any exceptions that occur during the execution
+        logger.exception(e)
+        raise e  # Reraise the exception for further handling
 
 
 # # Define the name of the current stage in the data processing pipeline
