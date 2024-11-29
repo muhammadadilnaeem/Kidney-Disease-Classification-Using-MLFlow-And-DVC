4930b043b9454a234112866282579b5e34909d1a
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-11-29 17:07:39 +0000

    contains constants params and config yaml file path

---
+++ src/kidney_disease_classifier/constants/__init__.py
@@ -0,0 +1,8 @@
+
+from pathlib import Path  # Import the Path class from the pathlib module for handling file paths
+
+# Define the path to the configuration file, which is expected to be located in the 'config' directory
+CONFIG_FILE_PATH = Path("config/config.yaml")
+
+# Define the path to the parameters file, which is expected to be in the same directory as the script
+PARAMS_FILE_PATH = Path("params.yaml")
